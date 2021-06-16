ca5cec3ff726a5a22e93e65019ffc3cdc8b59e47
Yousef Medhat <yousif.medhat562004@gmail.com>
2021-06-16 07:25:38 +0200

    add middlewares

--- app.js
+++ app.js
@@ -13,6 +13,11 @@ const PORT = process.env.PORT || 7777
 
 app.set('view engine', 'ejs')
 
+//Middlewares
+app.use(express.static('public'))
+app.use(express.urlencoded())
+app.use(express.json())
+
 //connect database
 mongoose.connect(process.env.db_url, { useUnifiedTopology: true, useNewUrlParser: true }, () => {
 
