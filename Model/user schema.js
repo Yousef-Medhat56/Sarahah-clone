3a1bac7f8e71101720103342f6d7851387f4a20d
Yousef Medhat <yousif.medhat562004@gmail.com>
2021-06-16 17:56:00 +0200

    check the validation of the email

--- Model/user schema.js
+++ Model/user schema.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose")
+const { isEmail } = require("validator")
 const { genSalt, hash } = require("bcryptjs")
 
 //create user schema
@@ -6,12 +7,13 @@ const userScema = new mongoose.Schema({
     username: {
         type: String,
         required: [true, 'Please enter your username'],
-        maxlength: [50, 'The username must be 50 characters at maximum']
+        maxlength: [50, 'The username must be 50 characters at maximum'],
     },
     email: {
         type: String,
         required: [true, 'Please enter your email'],
-        unique: true
+        unique: true,
+        validate: [isEmail, 'Please enter a valid email']
     },
     password: {
         type: String,
@@ -19,10 +21,7 @@ const userScema = new mongoose.Schema({
         minlength: [8, 'The password must be 8 characters at minimum'],
         maxlength: [20, 'The password must be 20 characters at maximum']
     },
-    gender: {
-        type: String,
-        required: [true, 'Please choose your gender']
-    }
+    gender: String
 })
 
 // hash the user password after sign up before creating his account
