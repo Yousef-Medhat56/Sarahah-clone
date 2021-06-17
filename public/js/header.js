b32c997a85f123fd646b66309b56ec27ab906348
Muhamed Safwat <84673283+Muhamedsafwat@users.noreply.github.com>
2021-06-17 12:55:26 +0200

    dark-mode modifications

--- public/js/header.js
+++ public/js/header.js
@@ -1,20 +1,28 @@
-const darkBtn = document.querySelector(".dark");
-const sun = document.querySelector(".dark .fa-sun");
-const moon = document.querySelector(".dark .fa-moon");
-const body = document.querySelector("body");
-const listBtn = document.querySelector(".list-icon");
-const list = document.querySelector("ul");
+  const darkBtn = document.querySelector(".dark");
+  const sun = document.querySelector(".dark .fa-sun");
+  const moon = document.querySelector(".dark .fa-moon");
+  const body = document.querySelector("body");
+  const listBtn = document.querySelector(".list-icon");
+  const list = document.querySelector("ul");
+  const text = document.getElementsByClassName("light-text");
+  const form = document.querySelector(".form");
 
-darkBtn.addEventListener("click", function () {
-  sun.classList.toggle("fadeIn");
-  sun.classList.toggle("fadeOut");
-  moon.classList.toggle("fadeIn");
-  moon.classList.toggle("fadeOut");
-  body.classList.toggle("dark-mode");
-  sun.classList.toggle("clicked");
-  moon.classList.toggle("clicked");
-  darkBtn.classList.toggle("button-clicked");
-});
-listBtn.addEventListener("click", function () {
-  list.classList.toggle("visible");
-});
+  darkBtn.addEventListener("click", function () {
+    sun.classList.toggle("fadeIn");
+    sun.classList.toggle("fadeOut");
+    moon.classList.toggle("fadeIn");
+    moon.classList.toggle("fadeOut");
+    body.classList.toggle("dark-mode");
+    sun.classList.toggle("clicked");
+    moon.classList.toggle("clicked");
+    darkBtn.classList.toggle("button-clicked");
+    for (word of text) {
+      word.classList.toggle("dark-text");
+    }
+    form.classList.toggle("light-form")
+  });
+  listBtn.addEventListener("click", function () {
+    list.classList.toggle("visible");
+  });
+
+  
--- public/js/signup.js
+++ public/js/signup.js
@@ -1,48 +1,44 @@
 // select DOM elements
-const formElm = document.querySelector("form")
+const formElm = document.querySelector("form");
 
+formElm.addEventListener("submit", async function (e) {
+  e.preventDefault();
+  try {
+    // create FormData object
+    const formData = new FormData(this);
 
-formElm.addEventListener("submit", async function(e) {
-    e.preventDefault()
-    try {
-        // create FormData object
-        const formData = new FormData(this)
+    //convert FormData object to reqular javascript object
+    const plainFormData = Object.fromEntries(formData.entries());
 
-        //convert FormData object to reqular javascript object
-        const plainFormData = Object.fromEntries(formData.entries());
+    //send the user data to the server
+    const response = await fetch("/signup", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+      body: JSON.stringify(plainFormData),
+    });
 
-        //send the user data to the server
-        const response = await fetch('/signup', {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-                Accept: "application/json",
-            },
-            body: JSON.stringify(plainFormData)
+    const data = await response.json();
 
-        })
+    //if the user input invalid data (make bad request)
+    if (response.status === 400) throw data; //throw the errors object as error
 
-        const data = await response.json()
+    console.log(data);
+  } catch (err) {
+    //create array for errors location
+    const errKeysArr = Object.keys(err); //["username","email","password","confirmPassword"]
 
-        //if the user input invalid data (make bad request)
-        if (response.status === 400) throw data //throw the errors object as error
+    let x = 0; // to control the numbers of shown errors to the user
+    for (let errKey of errKeysArr) {
+      this.querySelector(`.error.${errKey}`).textContent = ""; //reset all the error messages after each sumbit
 
-        console.log(data)
-
-    } catch (err) {
-        //create array for errors location 
-        const errKeysArr = Object.keys(err) //["username","email","password","confirmPassword"]
-
-        let x = 0 // to control the numbers of shown errors to the user
-        for (let errKey of errKeysArr) {
-            this.querySelector(`.error.${errKey}`).textContent = "" //reset all the error messages after each sumbit
-
-            //show the first error only in the error object
-            if (x < 1 && err[errKey]) {
-                this.querySelector(`.error.${errKey}`).textContent = err[errKey] //write the error message
-                x++ //to prevent any other error message from appearance in case there are more than one error
-            }
-
-        }
+      //show the first error only in the error object
+      if (x < 1 && err[errKey]) {
+        this.querySelector(`.error.${errKey}`).textContent = err[errKey]; //write the error message
+        x++; //to prevent any other error message from appearance in case there are more than one error
+      }
     }
-})
+  }
+});
