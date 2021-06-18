493f73a1b724b5c795674c29837c4db77ffde7e6
Muhamed Safwat <84673283+Muhamedsafwat@users.noreply.github.com>
2021-06-18 19:07:50 +0200

    added 404 page

--- public/js/header.js
+++ public/js/header.js
@@ -1,37 +1,30 @@
-  const darkBtn = document.querySelector(".dark");
-  const sun = document.querySelector(".dark .fa-sun");
-  const moon = document.querySelector(".dark .fa-moon");
-  const body = document.querySelector("body");
-  const listBtn = document.querySelector(".list-icon");
-  const list = document.querySelector("ul");
-  const text = document.getElementsByClassName("light-text");
-  const form = document.querySelector(".form");
-  const footer =document.querySelector(".footer");
-  const errMessage = document.getElementsByClassName("err-message")
+const darkBtn = document.querySelector(".dark");
+const sun = document.querySelector(".dark .fa-sun");
+const moon = document.querySelector(".dark .fa-moon");
+const body = document.querySelector("body");
+const listBtn = document.querySelector(".list-icon");
+const list = document.querySelector("ul");
+const text = document.getElementsByClassName("light-text");
+const form = document.querySelector(".form");
+const footer = document.querySelector(".footer");
 
-  darkBtn.addEventListener("click", function () {
-    sun.classList.toggle("fadeIn");
-    sun.classList.toggle("fadeOut");
-    moon.classList.toggle("fadeIn");
-    moon.classList.toggle("fadeOut");
-    body.classList.toggle("dark-mode");
-    sun.classList.toggle("clicked");
-    moon.classList.toggle("clicked");
-    darkBtn.classList.toggle("button-clicked");
-    for (word of text) {
-      word.classList.toggle("dark-text");
-    }
-    form.classList.toggle("background-light")
-    form.classList.toggle("background-dark")
-    footer.classList.toggle("background-light")
-    footer.classList.toggle("background-dark")
-    for(msg of errMessage) {
-      msg.classList.toggle("err-message-dark")
-    }
-    
-  });
-  listBtn.addEventListener("click", function () {
-    list.classList.toggle("visible");
-  });
-
-  
+darkBtn.addEventListener("click", function () {
+  sun.classList.toggle("fadeIn");
+  sun.classList.toggle("fadeOut");
+  moon.classList.toggle("fadeIn");
+  moon.classList.toggle("fadeOut");
+  body.classList.toggle("dark-mode");
+  sun.classList.toggle("clicked");
+  moon.classList.toggle("clicked");
+  darkBtn.classList.toggle("button-clicked");
+  for (word of text) {
+    word.classList.toggle("dark-text");
+  }
+  form.classList.toggle("background-light");
+  form.classList.toggle("background-dark");
+  footer.classList.toggle("background-light");
+  footer.classList.toggle("background-dark");
+});
+listBtn.addEventListener("click", function () {
+  list.classList.toggle("visible");
+});
