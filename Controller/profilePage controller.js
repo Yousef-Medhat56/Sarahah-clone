859aaa2f323a5b13ec54b63fe7ab7cf0781acff4
Yousef Medhat <yousif.medhat562004@gmail.com>
2021-06-24 14:18:29 +0200

    handle (delete account) request

--- Controller/profilePage controller.js
+++ Controller/profilePage controller.js
@@ -10,11 +10,16 @@ const profilePage_get = async(req, res, next) => {
         const authenticatedUser = req.user && req.user.userId
 
         //if who opens the profile page is authenticated owns the profile too, send to him ('account-owner-view') file
-        if (authenticatedUser === req.params.id) res.render('account-owner-view', { title: `Sarahah clone | ${user.username}`, username: user.username })
-            //if who opens the profile page isn't his owner
-        else res.render('account-visitor-view', { title: `Sarahah clone | ${user.username}`, username: user.username })
+        if (authenticatedUser === req.params.id) {
 
-    } catch (err) { next() }
+            res.render('account-owner-view', { title: `Sarahah clone | ${user.username}`, username: user.username, userImage: user.image })
+        }
+        //if who opens the profile page isn't his owner
+        else res.render('account-visitor-view', { title: `Sarahah clone | ${user.username}`, username: user.username, userImage: user.image })
+
+    } catch (err) {
+        next()
+    }
 
 }
 
@@ -23,4 +28,10 @@ const profilePage_logout = (req, res) => {
     res.cookie("refreshToken", '', { maxAge: 1 })
     res.send({ redirect: "/login" })
 }
-module.exports = { profilePage_get, profilePage_logout }
+
+const profilePage_delAccount = async(req, res) => {
+
+    await UserModel.findByIdAndDelete(req.params.id)
+    profilePage_logout(req, res)
+}
+module.exports = { profilePage_get, profilePage_logout, profilePage_delAccount }
--- Routes/profilePageRoutes.js
+++ Routes/profilePageRoutes.js
@@ -1,7 +1,7 @@
 const router = require("express").Router()
 
 const { verifyAccessToken, verifyRefreshToken } = require("../Controller/handlers/control jwt")
-const { profilePage_get, profilePage_logout } = require("../Controller/profilePage controller")
+const { profilePage_get, profilePage_logout, profilePage_delAccount } = require("../Controller/profilePage controller")
 
 //show about page
 router.get("/about", (req, res) => {
@@ -10,6 +10,7 @@ router.get("/about", (req, res) => {
 
 //logout the user
 router.delete("/logout", profilePage_logout)
+router.delete("/delete-account/:id", profilePage_delAccount)
 
 //profile page 
 router.get("/:id", verifyRefreshToken, verifyAccessToken, profilePage_get)
