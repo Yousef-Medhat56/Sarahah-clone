fd8f56f1b06bc570b4af273a5c96126790978624
Yousef Medhat <yousif.medhat562004@gmail.com>
2021-06-25 15:59:28 +0200

    check that the selected file format is valid to be the user image

--- public/js/sendPostRequest.js
+++ public/js/sendPostRequest.js
@@ -1,16 +1,30 @@
 // send POST request to change the user image
 const sendChangeImgReq = async(e) => {
-    //get the image 
-    const file = e.target.files[0]
-    const formData = new FormData
-    formData.append("image", file)
 
-    //send POST request
-    await fetch(`/image${location.pathname}`, {
-        method: "POST",
-        body: formData,
-    })
+    //valid image formats
+    const allowedExtensions =
+        /(\.jpg|\.jpeg|\.png)$/i;
 
-    //reload the page after getting the response
-    location.reload()
+    //check that the image format is valid
+    if (allowedExtensions.exec(e.target.value)) {
+        //get the image 
+        const file = e.target.files[0]
+        const formData = new FormData
+        formData.append("image", file)
+
+        //send POST request
+        await fetch(`/image${location.pathname}`, {
+            method: "POST",
+            body: formData,
+        })
+
+        //reload the page after getting the response
+        location.reload()
+    }
+    //if the file format is not valid
+    else {
+        //ignore the selected file
+        e.target.value = ''
+    }
+
 }
