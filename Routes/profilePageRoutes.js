efdba81f1c927d96d927ec5c7e05f16247e651c3
Yousef Medhat <yousif.medhat562004@gmail.com>
2021-06-29 15:02:18 +0200

    store the message in the database

--- Routes/profilePageRoutes.js
+++ Routes/profilePageRoutes.js
@@ -3,21 +3,24 @@ require("dotenv").config()
 
 //import middlewares
 const { verifyAccessToken, verifyRefreshToken } = require("../Controller/handlers/control jwt")
-const { profilePage_get } = require("../Controller/profilePage controller")
+const { profilePage_get, profilePage_patch } = require("../Controller/profilePage controller")
 
 
 
 
 
 //GET requests
-//about page
+//GET about page
 router.get("/about", (req, res) => {
     res.render("about", { title: "Sarahah clone | About" })
 })
 
 
-//profile page 
+//GET profile page 
 router.get("/:id", verifyRefreshToken, verifyAccessToken, profilePage_get)
 
+//PATCH
+//store message in the database
+router.patch('/sendMessage/:id', profilePage_patch)
 
 module.exports = router
